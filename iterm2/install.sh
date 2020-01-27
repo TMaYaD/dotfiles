50a398bc9f036fb2ee0e5be5dc815e473a1b97ee
Subhash Chandra <TMaYaD@LoonyB.in>
2020-01-27 16:25:12 +0530

    Added installation for iterm2 settings

---
+++ iterm2/install.sh
@@ -0,0 +1,8 @@
+if [ ! -n "$DOTFILES" ]; then
+  DOTFILES=~/.dotfiles
+fi
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DOTFILES/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
