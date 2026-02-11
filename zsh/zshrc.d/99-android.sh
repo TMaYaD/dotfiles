7dd805249554b8ea2f87266c75df2ef573f2e0a5
Subhash Chandra <TMaYaD+github@gmail.com>
2026-02-11 23:34:11 +0530

    feat: Add Android SDK and Java home environment variables to zsh configuration.

---
+++ zsh/zshrc.d/99-android.sh
@@ -0,0 +1,3 @@
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+export PATH="$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools"
+export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
