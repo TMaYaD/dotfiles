4752a475c28818345c7f54d0d5ffba5a889e79af
Subhash Chandra <TMaYaD+github@gmail.com>
2026-04-23 09:40:17 +0530

    fix(zsh): add .pub-cache/bin to path for flutter android apps.

--- zsh/zshrc.d/99-android.zsh
+++ zsh/zshrc.d/99-android.zsh
@@ -1,3 +1,4 @@
 export ANDROID_HOME="$HOME/Library/Android/sdk"
 export PATH="$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools"
 export JAVA_HOME="/Applications/Android Studio.app/Contents/jbr/Contents/Home"
+export PATH="$PATH":"$HOME/.pub-cache/bin"
